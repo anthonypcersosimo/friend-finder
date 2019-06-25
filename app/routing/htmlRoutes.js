371b957c8233a7f8c4f63f2ba92ff8a5a96bd2eb
Anthony <anthony.cersosimo@gmail.com>
2019-06-25 15:27:24 -0400

    updated the file paths in the htmlRoutes module

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,11 +2,10 @@ var path = require("path");
 
 var routeFunctions = {
     home: (res) => {
-        res.sendFile(path.join(__dirname, "../../app/public/home.html"));
-        res.sendFile(path.join(__dirname, "../../favicon.ico"));
+        res.sendFile(path.join(__dirname, "../public/home.html"));
     },
     survey: (res) => {
-        res.sendFile(path.join(__dirname, "../../app/public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     }
 };
 
