e60686a6ad69b07956b70cf7734bde7eebe5bc85
Anthony <anthony.cersosimo@gmail.com>
2019-06-24 23:21:36 -0400

    added favicon and added routes to server file

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening at http://localhost/" + PORT);
+});
