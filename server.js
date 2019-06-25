71c8a0be3f4319e8ca157471fb27f6d596a75c1d
Anthony <anthony.cersosimo@gmail.com>
2019-06-24 23:25:15 -0400

    sorry, NOW I added the routes to the server file

--- server.js
+++ server.js
@@ -12,6 +12,27 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Basic route that sends the user text when they hit the home path
+app.get("/index.html", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Route that contains JSON data of all users who booked a reservation
+app.get("/api/tables.html", function(req, res) {
+
+});
+
+// Matt do you wanna do the next .get below for the waitlist???
+app.get("/api/reservation.html",function(req,res) {
+    
+});
+
+// Now we need a post request when someone submits the form that Pat is building
+app.post("/api/reservation",function(req, res){
+
+})
+
+
 // Starts the server to begin listening
 // =============================================================
 app.listen(PORT, function() {
