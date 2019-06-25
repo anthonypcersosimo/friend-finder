753a4b85b81c805e25aa0c34293a0ed822d655e4
Anthony <anthony.cersosimo@gmail.com>
2019-06-25 14:46:57 -0400

    populated the lovers array with two fine gentlemen

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+var loversArray = [
+    {
+    "name":"Bill Nye",
+    "photo":"https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fewedit.files.wordpress.com%2F2015%2F01%2Fbill-nye-the-science-guy.jpg&w=400&c=sc&poi=face&q=85",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+    },
+    {
+    "name":"Jacob",
+    "photo":"https://research.jcu.edu.au/portfolio/mohan.jacob/photo/profile-x2/dffe44965149cb9cdd3adebd91b7fcf8",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+        ]
+    }
+      
+];
