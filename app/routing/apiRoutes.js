5083a812fd646e35967410275e838e329e3e4a3c
Anthony <anthony.cersosimo@gmail.com>
2019-06-26 22:09:43 -0400

    wrote out function for determining closest match to JSON of friends data

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,9 +5,36 @@ var apiFunctions = {
     },
     apiPost: (req, res) => {
         var friendsFunc = require('../data/friends');
-        var newFriend = req.body;
         friendsFunc.push(newFriend);
         console.log(friendsFunc);
+        
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: 10
+        };
+
+        var newFriend = req.body;
+        var friendScores = newFriend.scores;
+        console.log(friendScores);
+        var totalDifference = 0;
+
+        for (i = 0; i < friendsFunc.length; i++) {
+            console.log(friendsFunc[i]);
+            totalDifference = 0;
+
+            for (j = 0; j < friendsFunc[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(friendScores[j]) - parseInt(friendsFunc[i].scores[j]));
+
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friendsFunc[i].name;
+                    bestMatch.photo = friendsFunc[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                };
+            };
+        };
+
+        friendsFunc.push(newFriend);
     }
 };
 
--- server.js
+++ server.js
@@ -38,7 +38,6 @@ app.post("/api/friends",function(req, res){
     apiPostFunc.apiPost(req, res);
 })
 
-
 // Spin up server
 app.listen(PORT, function() {
     console.log("App listening at http://localhost/" + PORT);
